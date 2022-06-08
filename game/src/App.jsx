0418149316d273a7c3075af3e6b3997fd00009a8
Hussain Jawad <45372216+HusJAW@users.noreply.github.com>
2022-06-08 14:37:34 +0100

    update

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 import PageLayout from "./components/PageLayout";
 import x from "./components/GameLogic.js";
 import NavBar from "./components/NavBar";
+import GameCard from "./components/GameCard";
 
 function App() {
   return (
     <PageLayout>
       <NavBar />
+      <GameCard>a</GameCard>
     </PageLayout>
   );
 }
