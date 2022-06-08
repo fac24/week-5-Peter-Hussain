e4de83b509acda44faac6bb97c4d7bb2d46e1aa5
Hussain Jawad <45372216+HusJAW@users.noreply.github.com>
2022-06-08 14:51:23 +0100

    update

--- game/src/components/GameLogic.js
+++ game/src/components/GameLogic.js
@@ -1,3 +1,18 @@
 export default function X() {
   console.log("hi");
 }
+
+let apiImg = [
+  {id:1, img:imgURl}
+  {id:4, img:imgURl}
+  {id:3, img:imgURl}
+  {id:2, img:imgURl}
+]
+
+let gameArray = apiImg.concat(apiImg)
+
+//shuffle the array 
+
+// render the shufffled array into cards each card will take id + img  
+// if id matches keep the card displied else flipback 
+//
