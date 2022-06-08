4d7d706cf65855a31a760d10aefdb73d26482581
Hussain Jawad <45372216+HusJAW@users.noreply.github.com>
2022-06-08 14:42:32 +0100

    update

--- game/src/components/NavBar.jsx
+++ game/src/components/NavBar.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function NavBar() {
   return (
-    <heade className="navbar">
+    <header className="navbar">
       <h1 className="logo">broccoli</h1>
       <a className="restart-button" href="/">
         Restart
       </a>
-    </heade>
+    </header>
   );
 }
