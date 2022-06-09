3b04a0ffe0c238ec9a4eca697b9983b6a6c4b3d1
Hussain Jawad <45372216+HusJAW@users.noreply.github.com>
2022-06-09 13:12:50 +0100

    grab user image Co-authored-by: Peter James Salter <PJSalter@users.noreply.github.com>

--- game/src/components/GameCardContainer.jsx
+++ game/src/components/GameCardContainer.jsx
@@ -1,26 +1,62 @@
 import React from "react";
 import GameCard from "./GameCard";
 
+let userUrl = [];
+
+async function fetchUser(user) {
+  fetch(`https://api.github.com/users/${user}`)
+    .then((res) => res.json())
+    .then((data) => userUrl.push(data));
+}
+let githubUsers = [
+  "sofer",
+  "reubengt",
+  "oliverjam",
+  "Albadylic",
+  "lascellesabercrombie",
+  "Bereketmebrahtu",
+  "minju25kim",
+  "AsmahanM",
+];
+
+githubUsers.map((user) => {
+  fetchUser(user);
+});
+
 export default function GameCardContainer() {
+  const [username, setUsername] = React.useState(userUrl);
+
   return (
     <div className="game-card-container">
-      <GameCard>1</GameCard>
-      <GameCard>2</GameCard>
-      <GameCard>3</GameCard>
-      <GameCard>4</GameCard>
-      <GameCard>5</GameCard>
-      <GameCard>6</GameCard>
-      <GameCard>7</GameCard>
-      <GameCard>8</GameCard>
+      <GameCard id="1" key="1" img={userUrl[0].avatar_url} />
+      <GameCard id="1" key="1" img={userUrl[1].avatar_url} />
+      <GameCard id="1" key="1" img={userUrl[2].avatar_url} />
+      <GameCard id="1" key="1" img={userUrl[3].avatar_url} />
+      <GameCard id="1" key="1" img={userUrl[4].avatar_url} />
+      <GameCard id="1" key="1" img={userUrl[5].avatar_url} />
+      <GameCard id="1" key="1" img={userUrl[6].avatar_url} />
+      <GameCard id="1" key="1" img={userUrl[7].avatar_url} />
 
-      <GameCard>1</GameCard>
-      <GameCard>2</GameCard>
-      <GameCard>3</GameCard>
-      <GameCard>4</GameCard>
-      <GameCard>5</GameCard>
-      <GameCard>6</GameCard>
-      <GameCard>7</GameCard>
-      <GameCard>8</GameCard>
+      <GameCard id="1" key="1" img={userUrl[0].avatar_url} />
+      <GameCard id="1" key="1" img={userUrl[1].avatar_url} />
+      <GameCard id="1" key="1" img={userUrl[2].avatar_url} />
+      <GameCard id="1" key="1" img={userUrl[3].avatar_url} />
+      <GameCard id="1" key="1" img={userUrl[4].avatar_url} />
+      <GameCard id="1" key="1" img={userUrl[5].avatar_url} />
+      <GameCard id="1" key="1" img={userUrl[6].avatar_url} />
+      <GameCard id="1" key="1" img={userUrl[7].avatar_url} />
     </div>
   );
 }
+
+// if (userUrl.length === 8) {
+//   return (
+//     <div className="game-card-container">
+//       {userUrl.map((item, key) => (
+//         <GameCard key={key} img={item.avatar_url} />
+//       ))}
+//     </div>
+//   );
+// } else {
+//   return <div className="game-card-container">loading</div>;
+// }
--- game/src/components/GameLogic.js
+++ game/src/components/GameLogic.js
@@ -1,18 +1,47 @@
-export default function X() {
-  console.log("hi");
-}
+// export default function GameLogic({ githubUsers }) {
+//   console.log("hi");
+// }
 
-let apiImg = [
-  { id: 1, img: imgURl },
-  { id: 4, img: imgURl },
-  { id: 3, img: imgURl },
-  { id: 2, img: imgURl },
-];
+// //shuffle the array
 
-let gameArray = apiImg.concat(apiImg);
+// // render the shufffled array into cards each card will take id + img
+// // if id matches keep the card displied else flipback
 
-//shuffle the array
+// let githubUsers = [
+//   "sofer",
+//   "reubengt",
+//   "oliverjam",
+//   "Albadylic",
+//   "lascellesabercrombie",
+//   "Bereketmebrahtu",
+//   "minju25kim",
+//   "AsmahanM",
+// ];
 
-// render the shufffled array into cards each card will take id + img
-// if id matches keep the card displied else flipback
-//
+// let id = 0;
+
+// let userURL = [];
+
+// let jsxArray = [];
+
+// githubUsers
+//   .forEach((user) =>
+//     React.useEffect(() => {
+//       fetch(`https://api.github.com/users/${user}`)
+//         .then((res) => res.json())
+//         .then((data) => setObj(data))
+//         .then(() => userURL.push({ id: id++, userUrl: obj.avatar_url }));
+//     }, [obj])
+//   )
+//   .then(() =>
+//     userURL.forEach((user) =>
+//       gitHubUsers.push(<GameCard id={user.id} img={user.avatar_url} />)
+//     )
+//   );
+
+// console.log(jsxArray, "jsx array");
+// console.log(userURL, "userurl array");
+
+// // if (jsxArray.length === 8) {
+// //   return <div className="game-card-container">{jsxArray}</div>;
+// // }
