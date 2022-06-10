c45ef7a6478eb96f6b1ac9dfe1e6c45773e811e4
Hussain Jawad <45372216+HusJAW@users.noreply.github.com>
2022-06-10 14:14:37 +0100

    broccoli
    co-authored-by: Peter <PJSalter@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import PageLayout from "./components/PageLayout";
 import GameCardContainer from "./components/GameCardContainer";
+import Broccoli from "./components/Broccoli";
 
 function App() {
   return (
