ef624faaa874ce1758c9914a680e72f9997bb51e
Hussain Jawad <45372216+HusJAW@users.noreply.github.com>
2022-06-10 10:48:22 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,14 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import PageLayout from "./components/PageLayout";
 import GameCardContainer from "./components/GameCardContainer";
-import Broccoli from "./components/broccoli";
+// import Broccoli from "./components/broccoli";
 
 function App() {
   return (
-    <>
-      <PageLayout>
-        <NavBar />
-        <GameCardContainer />
-      </PageLayout>
-
-      <Broccoli />
-    </>
+    <PageLayout>
+      <NavBar />
+      <GameCardContainer />
+    </PageLayout>
   );
 }
 
